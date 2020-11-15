66e9465358ee73b82e035ce53560a94f69098871
EidAbdullahi <eidabdullahi10@gmail.com>
2020-11-15 10:14:27 +0300

    commit changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+// import {HttpClient} from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+// import { HttpHeaderResponse } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ProfileComponent } from './profile/profile.component';
+// import { ProfileService } from './profile.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProfileComponent,
     AboutComponent,
-    NavbarComponent,
-    ProfileComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    // HttpClient,
+    // HttpHeaderResponse
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/profile.service.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService implements OnInit{
+  private clientid = 'Iv1.b39a9ac1fc56ff9d';
+  private clientsecret = ' 6bf06bf90964d4b26d7518e90aaaf35c5cc7c0a9';
+
+  constructor(private http: HttpClient) {
+    console.log("service is now ready");
+   }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+   getProfileInfo(username){
+    interface apiUrl {
+      login: string,
+      avatar_url: string,
+      blog:string,
+      name:string,
+      public_repos: number,
+      followers: number,
+      following: number,
+      }
+      return this.http.get<apiUrl>("https://api.github.com/users/" + username + "?client_id=" +
+      this.clientid + "&client_secret=" + this.clientsecret);     
+   }
+  
+    getProfileRepos(username){
+    interface apiUrl {
+      name: string,
+      html_url: string,
+      description:string
+      }
+    return this.http.get<apiUrl>("https://api.github.com/users/" + username + "/repos?client_id=" +
+     this.clientid + "&client_secret=" + this.clientsecret);    
+   }
+}
+// import { Component, OnInit } from '@angular/core';
+// import {HttpClient} from '@angular/common/http'
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ProfileService implements OnInit{
+//   private clientid = 'Iv1.2c7f97d2a9af5a15';
+//   private clientsecret = 'db5d9ff5932b00216facebbce52d0b5d4e3fd4e1';
+
+//   constructor(private http: HttpClient) {
+//     console.log("service is now ready");
+//    }
+//   ngOnInit(): void {
+//     throw new Error('Method not implemented.');
+//   }
+
+//    getProfileInfo(username){
+//     interface apiUrl {
+//       login: string,
+//       avatar_url: string,
+//       blog:string,
+//       name:string,
+//       public_repos: number,
+//       followers: number,
+//       following: number,
+//       }
+//      return this.http.get<apiUrl>("https://api.github.com/users/" + username + "?client_id=" +
+//       this.clientid + "&client_secret=" + this.clientsecret);    
+//    }
+
+   
+//    getProfileRepos(username){
+//     interface apiUrl {
+//       name: string,
+//       html_url: string,
+//       description:string
+//       }
+//     return this.http.get<apiUrl>("https://api.github.com/users/" + username + "/repos?client_id=" +
+//      this.clientid + "&client_secret=" + this.clientsecret);    
+//    }
+// }
